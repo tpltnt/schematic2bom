16118ff9bbbb23b7a9528612134db9e762ca6b55
tpltnt <tpltnt.github@dropcut.net>
2012-03-27 13:48:30 +0200

    line to csv done

--- schematic2bom.rb
+++ schematic2bom.rb
@@ -6,11 +6,20 @@
 
 schematicfilename = ARGV[0]
 schematic = File.open(schematicfilename, "r")
+csvline = ""
 
 schematic.each do |line|
+	# create one CSV line per component
+	if 0 == line.index("$Comp") then
+		csvline = ""
+	elsif 0 == line.index("$EndComp") then
+		puts csvline.chop.chop
+	end
+	
 	# F 4 "<FIELD CONTENT>" H 7350 2900 60  0001 C CNN "<FIELD NAME>"
 	if 70 == line[0] then
-		puts line
+		csvline = csvline + line.split("\"")[1] + ", "
+		#csvline = csvline + line.split(" ")[10]
 	end
 end
 
