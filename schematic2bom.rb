3ccdb059fbafbd8ca771a491e5ec418ece3b8d76
tpltnt <tpltnt.github@dropcut.net>
2012-03-27 13:32:52 +0200

    filtering field lines done

--- schematic2bom.rb
+++ schematic2bom.rb
@@ -8,7 +8,10 @@ schematicfilename = ARGV[0]
 schematic = File.open(schematicfilename, "r")
 
 schematic.each do |line|
-	puts line
+	# F 4 "<FIELD CONTENT>" H 7350 2900 60  0001 C CNN "<FIELD NAME>"
+	if 70 == line[0] then
+		puts line
+	end
 end
 
 schematic.close
